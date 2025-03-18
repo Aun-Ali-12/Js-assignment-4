df30e3ba31db5af8e724b638751116fecdbcb493
Aun-Ali-12 <aunali29331@gmail.com>
2025-03-18 09:13:34 +0500

    Layout created but logic remained non-workable

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const myForm = document.querySelector('my-form')
-const firstNum = document.querySelector('num-1')
-const secNum = document.querySelector('num-2')
-const resultpara = document.querySelector('result')
+const myForm = document.querySelector("my-form");
+const firstNum = document.querySelector("num-1");
+const secNum = document.querySelector("num-2");
+const resultpara = document.querySelector("result");
+const btn = document.querySelector("#btn");
 
-myForm.addEventListener("submit",(event) =>{
-    event.preventDefault();
-    console.log('Form is submitted')
+myForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Form is submitted");
 
-    const num1 = Number(firstNum.value)
-    const num2 = Number(secNum.value)
-    
-    const sum = num1 + num2
-    console.log(sum);
-    
-    resultpara.innerText = sum
+  const num1 = Number(firstNum.value);
+  const num2 = Number(secNum.value);
 
-})
+  const sum = num1 + num2;
+  console.log(sum);
+
+  resultpara.innerText = sum;
+});
